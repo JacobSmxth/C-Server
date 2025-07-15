ef4f113e6dd2521c7535932a4d323a9f8c0adf15
jacobsmxth <jacob.d.smith@live.com>
2025-07-15 00:13:28 -0400

    Added some useful information I learned regarding network sockets

--- src/main.c
+++ src/main.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+#include <sys/socket.h> // Man page told me about this. Reading up on beej's guide!
+
+// Stuff I learn
+//
+// Everything in UNIX is a file
+//
+// Everything gors through a file descriptor
+// For network communication you would use the socket system routine
+// Since this is a file descriptor, technically you can use read() and write() but send() and recv() give better control over data transmission.
+//
+// Three Kinds of sockets:
+// DARPA Internet addresses (Internet Sockets)
+// Path names on a local node (UNIX Sockets)
+// CCITT X.25 addresses (X.25 Sockets (can safely ignore))
+//
+// I WILL FOCUS ON INTERNET SOCKETS
+//
+// (I will continue later as its currently 12;13 am and I need sleep after long day)
+
 
 
 int main() {
